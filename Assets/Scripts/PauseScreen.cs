dbc03b72a1a7403e4dbdf8a1283398dfd309afc7
LizaA-hub <150455028+LizaA-hub@users.noreply.github.com>
2025-01-14 14:47:06 +0100

    pause screen
    possibility to pause in game by pressing esc

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    [SerializeField]
+    Transform panel;
+    bool isOpen = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePanel();
+        }
+    }
+
+    public void TogglePanel()
+    {
+        isOpen = !isOpen;
+        panel.gameObject.SetActive(isOpen);
+        Time.timeScale = isOpen ? 0.0f : 1.0f;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        GameManager.LoadMenu();
+    }
+
+}
